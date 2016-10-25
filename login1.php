e52d003e6238460d47922c4ee608d64d1a9dc381
Raveesh Taneja <raveeshtaneja@gmail.com>
2016-10-25 23:40:19 +0530

    las1t deployment commit

--- login1.php
+++ login1.php
@@ -9,7 +9,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Sign-Up/Login Form</title>
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
     
     <link rel="stylesheet" href="css/normalize.css">
 
